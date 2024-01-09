6abab711f3290557742d59b9a22912c31d6e0169
Muammar Zaidan Ariq <zaidanariq503@gmail.com>
2024-01-09 21:01:45 +0700

    initial commit

--- index.js
+++ index.js
@@ -16,15 +16,23 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+	res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+	const ipaddress = req.ip;
+	const language = req.headers['accept-language'];
+	const software = req.headers['user-agent'];
+
+	res.json({ ipaddress, language, software });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
